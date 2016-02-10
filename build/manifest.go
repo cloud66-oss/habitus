1993a10caa53a6ac09b1b2df1693327105aab809
Khash Sajadi <khash@cloud66.com>
2016-02-09 16:37:44 -0800

    service dependency logic

--- build/manifest.go
+++ build/manifest.go
@@ -1,6 +1,7 @@
 package build
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -14,7 +15,6 @@ import (
 
 // Artefact holds a parsed source for a build artefact
 type Artefact struct {
-	Order  int
 	Step   Step
 	Source string
 	Dest   string // this is only the folder. Filename comes from the source
@@ -28,12 +28,12 @@ type Cleanup struct {
 // Step Holds a single step in the build process
 // Public structs. They are used to store the build for the builders
 type Step struct {
-	Order      int
 	Name       string
 	Dockerfile string
 	Artefacts  []Artefact
 	Manifest   Manifest
 	Cleanup    *Cleanup
+	DependsOn  []*Step
 }
 
 // Manifest Holds the whole build process
@@ -52,8 +52,10 @@ type step struct {
 	Dockerfile string
 	Artefacts  []string
 	Cleanup    *cleanup
+	DependsOn  []string
 }
 
+// This is loaded from the build.yml file
 type build struct {
 	Workdir string
 	Steps   []step
@@ -81,18 +83,28 @@ func LoadBuildFromFile(config *configuration.Config) (*Manifest, error) {
 	return t.convertToBuild()
 }
 
+// finds a step in the loaded build.yml by name
+func (b *build) findStepByName(name string) (*step, error) {
+	for _, step := range b.Steps {
+		if step.Name == name {
+			return &step, nil
+		}
+	}
+
+	return nil, nil
+}
+
 func (b *build) convertToBuild() (*Manifest, error) {
 	r := Manifest{}
 	r.IsPrivileged = false
 	r.Steps = []Step{}
 
-	for idx, s := range b.Steps {
+	for _, s := range b.Steps {
 		convertedStep := Step{}
 
 		convertedStep.Manifest = r
 		convertedStep.Dockerfile = s.Dockerfile
 		convertedStep.Name = s.Name
-		convertedStep.Order = idx
 		convertedStep.Artefacts = []Artefact{}
 		if s.Cleanup != nil && !b.Config.NoSquash {
 			convertedStep.Cleanup = &Cleanup{Commands: s.Cleanup.Commands}
@@ -101,10 +113,9 @@ func (b *build) convertToBuild() (*Manifest, error) {
 			convertedStep.Cleanup = &Cleanup{}
 		}
 
-		for kdx, a := range s.Artefacts {
+		for _, a := range s.Artefacts {
 			convertedArt := Artefact{}
 
-			convertedArt.Order = kdx
 			convertedArt.Step = convertedStep
 			parts := strings.Split(a, ":")
 			convertedArt.Source = parts[0]
@@ -128,7 +139,93 @@ func (b *build) convertToBuild() (*Manifest, error) {
 		r.Steps = append(r.Steps, convertedStep)
 	}
 
+	// now that we have the Manifest built from the file, we can resolve dependencies
+	for idx, s := range r.Steps {
+		bStep, err := b.findStepByName(s.Name)
+		if err != nil {
+			return nil, err
+		}
+		if bStep == nil {
+			return nil, fmt.Errorf("step not found %s", s.Name)
+		}
+
+		for _, d := range bStep.DependsOn {
+			convertedStep, err := r.FindStepByName(d)
+			if err != nil {
+				return nil, err
+			}
+			if convertedStep == nil {
+				return nil, fmt.Errorf("can't find step %s", d)
+			}
+
+			r.Steps[idx].DependsOn = append(r.Steps[idx].DependsOn, convertedStep)
+		}
+	}
+
+	// build the dependency tree
+	_, err := r.serviceOrder(r.Steps)
+	if err != nil {
+		return nil, err
+	}
+
 	return &r, nil
+}
+
+// takes in a list of steps and returns an array of steps ordered by their dependency order
+// result[0] will be an array of all steps with no dependency
+// result[1] will be an array of steps depending on one or more of result[0] steps and so on
+func (m *Manifest) serviceOrder(list []Step) ([][]Step, error) {
+	if len(list) == 0 {
+		return [][]Step{}, nil
+	}
+
+	var result [][]Step
+
+	// find all steps with no dependencies
+	for {
+		var level []Step
+		for _, step := range list {
+			if len(step.DependsOn) == 0 {
+				level = append(level, step)
+			}
+		}
+
+		// if none is found while there where items in the list, then we have a circular dependency somewhere
+		if len(list) != 0 && len(level) == 0 {
+			return nil, errors.New("Found circular dependency in services")
+		}
+
+		result = append(result, level)
+
+		// now take out all of those found from the list of other items (they are now 'resolved')
+		for idx, step := range list { // for every step
+			stepDeps := append([]*Step(nil), step.DependsOn...) // clone the dependency list so we can remove items from it
+			for kdx, dep := range stepDeps {                    // iterate through its dependeneis
+				for _, resolved := range level { // and find any resolved step in them and take it out
+					if resolved.Name == dep.Name {
+						list[idx].DependsOn = append(list[idx].DependsOn[:kdx], list[idx].DependsOn[kdx+1:]...)
+					}
+				}
+			}
+		}
+
+		// take out everything we have in this level from the list
+		for _, s := range level {
+			listCopy := append([]Step(nil), list...)
+			for idx, l := range listCopy {
+				if s.Name == l.Name {
+					list = append(list[:idx], list[idx+1:]...)
+				}
+			}
+		}
+
+		// we are done
+		if len(list) == 0 {
+			break
+		}
+	}
+
+	return result, nil
 }
 
 // FindStepByName finds a step by name. Returns nil if not found
