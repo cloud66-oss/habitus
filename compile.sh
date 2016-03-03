fc1d3dd75a50bde5129480c15c7a8316b566b867
Khash Sajadi <khash@cloud66.com>
2016-03-03 14:29:30 +0000

    allow dynamic versions during compile

--- compile.sh
+++ compile.sh
@@ -1,1 +1,7 @@
-gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -output="compiled/{{.Dir}}_{{.OS}}_{{.Arch}}"
+if [ $# -eq 0 ]
+  then
+    echo "No version supplied"
+    exit 1
+fi
+
+gox -ldflags="-X main.VERSION=$1" -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -output="compiled/{{.Dir}}_{{.OS}}_{{.Arch}}"
