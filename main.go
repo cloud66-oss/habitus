bc3eea24036894cee1d264f6c87130444b5e61f6
Andreas Galanomatis <andreas@cloud66.com>
2016-03-04 16:50:32 +0000

    accidentally deleted no-cleanup

--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func main() {
 	flag.StringVar(&config.DockerCert, "certs", os.Getenv("DOCKER_CERT_PATH"), "Docker cert folder. Uses DOCKER_CERT_PATH if missing")
 	flag.Var(&config.EnvVars, "env", "Environment variables to be used during build. Uses parent process environment variables if empty")
 	flag.BoolVar(&config.KeepSteps, "keep-all", false, "Overrides the keep flag for all steps. Used for debugging")
+	flag.BoolVar(&config.NoSquash, "no-cleanup", false, "Skip cleanup commands for this run. Used for debugging")
 	flag.BoolVar(&config.FroceRmImages, "force-rmi", false, "Force remove of unwanted images")
 	flag.BoolVar(&config.NoPruneRmImages, "noprune-rmi", false, "No pruning of unwanted images")
 	flag.BoolVar(&flagShowHelp, "help", false, "Display the help")
