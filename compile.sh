85b66993f8b931f12333fb47fd7296f2925ee086
Khash Sajadi <khash@cloud66.com>
2016-03-15 10:41:36 +0000

    remove wrong merge

--- compile.sh
+++
@@ -1,1 +0,0 @@
-gox -osarch="darwin/386 darwin/amd64 linux/386 linux/amd64 windows/386 windows/amd64" -output="compiled/{{.Dir}}_{{.OS}}_{{.Arch}}"
--- gnu-tar.md
+++
@@ -1,7 +0,0 @@
-Install GNU tar on OS X
-
-```bash
-brew install gnu-tar
-PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
-```
-
