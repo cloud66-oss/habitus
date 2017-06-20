710ca2c08ed921e18da88bd09892d28b4006eb8c
Daniel van Gils <daniel@cloud66.com>
2017-06-20 10:07:48 +0200

    adding version of habitus to install script and show progress bar

--- habitus_install.sh
+++ habitus_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+version=1.0.0
+
 # bash utilities credit: http://natelandau.com/bash-scripting-utilities/
+
 
 #Formatting output
 bold=$(tput bold)
@@ -41,16 +44,16 @@ do_install() {
   if is_os "darwin"; then
     e_info "Downloading Habitus for macOS..."
     `rm -f /tmp/habitus &> /dev/null`
-    `curl -L -o /tmp/habitus -s https://github.com/cloud66/habitus/releases/download/1.0.0/habitus_macosx`
+    `curl -L --progress-bar -o /tmp/habitus https://github.com/cloud66/habitus/releases/download/$version/habitus_darwin_amd64`
   elif is_os "linux"; then
     if [[ is_64 ]]; then
 	  e_info "Downloading Habitus for Linux x64..."
-	  `rm -f /tmp/c66_toolbelt.tar.gz &> /dev/null`
-	  `curl -L -o /tmp/habitus -s https://github.com/cloud66/habitus/releases/download/1.0.0/habitus_linux`
+	  `rm -f /tmp/habitus &> /dev/null`
+	  `curl -L --progress-bar -o /tmp/habitus https://github.com/cloud66/habitus/releases/download/$version/habitus_linux_amd64`
     else
 	  e_info "Downloading Habitus for Linux x32..."
-	  `rm -f /tmp/c66_toolbelt.tar.gz &> /dev/null`
-	  `curl -L -o /tmp/habitus -s https://github.com/cloud66/habitus/releases/download/1.0.0/habitus_linux`
+	  `rm -f /tmp/habitus &> /dev/null`
+	  `curl -L --progress-bar -o /tmp/habitus https://github.com/cloud66/habitus/releases/download/$version/habitus_linux_386`
     fi
   else
   	e_error "Aborted: Unable to detect your operating system and architecture!"
@@ -73,14 +76,14 @@ do_install() {
   `ln -nfs $USER_HOME/.habitus/habitus /usr/local/bin/habitus &> /dev/null`
   if [ $? -eq 0 ] ; then
   	e_info "The 'habitus' command should now be available"
-  	e_success "YAY! Successfully installed Habitus!"
+  	e_success "Successfully installed Habitus! Go build some images!"
   else
 	e_warning "Warning: Unable to create a symlink for Habitus"
 	e_warning "Please create your symlink manually from $USER_HOME/.habitus/habitus"
   fi
 }
 
-e_success "Installing Habitus V1.0.0"
+e_success "Installing Habitus V$version"
 # check if running as sudoer
 if [ $UID -eq 0 ] ; then
 	USER_HOME="/home/"$SUDO_USER
